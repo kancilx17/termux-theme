10e298833f3e5aa690df9cf33829e07095d8c30f
-=[ Say Dog ]=- <55991841+saydog@users.noreply.github.com>
2019-11-06 04:21:31 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg update && pkg upgrade -y
+termux-setup-storage
+pkg install -y ruby figlet screenfetch
+pkg install -y python2 nano mc
+gem install lolcat
+
+apt install -y git zsh
+git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1
+
+mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+cp -R "$HOME/termux-ohmyzsh/.termux" "$HOME/.termux"
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
+mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
+sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
+sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
+echo "alias chcolor='$HOME/.termux/colors.sh'" >> "$HOME/.zshrc"
+echo "alias chfont='$HOME/.termux/fonts.sh'" >> "$HOME/.zshrc"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
+chsh -s zsh
+
+mkdir ~/theme-zsh
+chmod +x theme.py
+cat def-zsh > ~/.zshrc
+rm install.sh
+python2 theme.py
